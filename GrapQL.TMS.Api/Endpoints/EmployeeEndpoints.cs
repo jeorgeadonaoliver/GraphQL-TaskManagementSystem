1c84721e38e5205a069657c5851d94c0a243dfd2
Jeorge Oliver <jeorgeadonaoliver@gmail.com>
2025-06-04 23:55:24 +0800

    Commit 1

--- GrapQL.TMS.Api/Endpoints/EmployeeEndpoints.cs
+++ GrapQL.TMS.Api/Endpoints/EmployeeEndpoints.cs
@@ -1,4 +1,6 @@
-﻿using GraphQL.TMS.Application.Features.EmployeeManagement.Query.GetEmployee;
+﻿using GraphQL.TMS.Application.Features.EmployeeManagement.Command.CreateEmployee;
+using GraphQL.TMS.Application.Features.EmployeeManagement.Command.UpdateEmployee;
+using GraphQL.TMS.Application.Features.EmployeeManagement.Query.GetEmployee;
 using MediatR;
 
 namespace GraphQL.TMS.Api.Endpoints
@@ -16,6 +18,28 @@ namespace GraphQL.TMS.Api.Endpoints
             .WithName("GetEmployee")
             .WithTags("Employee")
             .Produces<List<GetEmployeeQueryDto>>(StatusCodes.Status200OK);
+
+            app.MapPost("/employee", async (IMediator mediator, CreateEmployeeCommand command) =>
+            {
+
+                var createEmployee = await mediator.Send(command);
+                return Results.Ok(createEmployee);
+            })
+            .WithName("CreateEmployee")
+            .WithTags("Employee")
+            .Produces(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+
+            app.MapPut("/employee", async (IMediator mediator, UpdateEmployeeCommand command) =>
+            {
+
+                var updateEmployee = await mediator.Send(command);
+                return Results.Ok(updateEmployee);
+            })
+            .WithName("UpdateEmployee")
+            .WithTags("Employee")
+            .Produces(StatusCodes.Status202Accepted)
+            .ProducesProblem(StatusCodes.Status400BadRequest); 
 
             return app;
         }
